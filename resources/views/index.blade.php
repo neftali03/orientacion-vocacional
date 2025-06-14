716edbf6dbc7c24b119603bbb5b0f367965f55bd
neftali <neftali@depotelas.com>
2025-06-14 11:32:04 -0600

    Fix some details index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.base')
 
 @section('content')
-<div class="py-5">
-    <div class="container text-center py-5">
+<div class="">
+    <div class="container text-center py-4">
         <div class="row align-items-start">
-            <div class="col-12 col-md-6 order-1 mt-5 py-5 d-flex justify-content-center">
-                <img src="{{ asset('LogoITCA.png') }}" style="height: 135px" alt="Imagen">
+            <div class="col-12 col-md-5 order-1 mt-5 py-5 d-flex justify-content-center">
+                <img src="{{ asset('LogoITCA.png') }}" style="height: 131px" alt="Imagen">
             </div>
-            <div class="col-12 col-md-6 order-2 order-md-1">
+            <div class="col-12 col-md-7 order-2 order-md-1">
                 <h1 class="display-2 fw-bold">¿Estás listo</h1>
-                <h4 class="display-6 fw-bold">para tu</h4>
+                <h4 class="display-6 fw-bold text-orange">para tu</h4>
                 <h1 class="display-2 fw-bold">test vocacional?</h1>
-                <p class="fs-4 py-5">
+                <p class="fs-4 py-4">
                     ¿Tienes dudas sobre qué carrera estudiar? ¡No te preocupes!
                     Realiza nuestro test vocacional y te ayudaremos a orientarte
                     para elegir la carrera ideal para ti.
