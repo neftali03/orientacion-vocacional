c80c37bbd6914dff0234fb46c3a1394c327c2bf6
neftali <neftali@depotelas.com>
2025-06-25 15:14:27 -0600

    Add HTTPS to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -25,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+        
         View::composer('*', function ($view) {
             $view->with('authUser', Auth::user());
             $view->with('hasuraUserId', session('hasura_user_id'));
