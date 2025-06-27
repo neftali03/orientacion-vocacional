09d6ca872177fa435364cdfdf51d1b88065df490
neftali <neftali@depotelas.com>
2025-06-27 15:55:17 -0600

    Change asset base

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Orientación vocacional</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-    <script src="{{ asset('js/bootstrap/bootstrap.bundle.js') }}" defer></script>
+    <link rel="icon" type="image/x-icon" href="{{ secure_asset('favicon.ico') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/main.css') }}">
+    <script src="{{ secure_asset('js/bootstrap/bootstrap.bundle.js') }}" defer></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body hx-ext="loading-states">
@@ -27,7 +27,7 @@
     </div>
     @include('common._toasts')
     @stack('modals')
-    <script src="{{ asset('js/base.js') }}" defer></script>
+    <script src="{{ secure_asset('js/base.js') }}" defer></script>
     @yield('scripts')
 </body>
 </html>
